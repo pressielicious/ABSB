01f9b703651f907c8a4cb9067b7c6793d509c5c2
pressielicious <pressiepotato.exe@gmail.com>
2025-02-25 19:38:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,8 @@ const map = L.map('map', {
     maxZoom: 1
 }).setView([0, 0], -1);
 
-const imageUrl = "map.jpeg";  // Tukar kepada fail peta GTA V
-const imageBounds = [[-12888, -8192], [12888, 8192]];
-
+const imageUrl = "peta-gta-v.jpg";  // Tukar kepada fail peta GTA V
+const imageBounds = [[-8192, -6444], [8192, 6444]];
 L.imageOverlay(imageUrl, imageBounds).addTo(map);
 map.setMaxBounds(imageBounds);
 
