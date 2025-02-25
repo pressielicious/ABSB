047ae081a3fc03c8ad1a1c25c1d34949f4330101
pressielicious <pressiepotato.exe@gmail.com>
2025-02-25 18:08:03 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyCMk2cfQZjEkOxReerdDeAIzxS-hHLbsow",
+    authDomain: "abrp-sb-15c1b.firebaseapp.com",
+    databaseURL: "https://abrp-sb-15c1b-default-rtdb.firebaseio.com",
+    projectId: "abrp-sb-15c1b",
+    storageBucket: "abrp-sb-15c1b.firebasestorage.app",
+    messagingSenderId: "629320985348",
+    appId: "1:629320985348:web:4aedc33343dfe521637e51"
 };
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
