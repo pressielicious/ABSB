6ed2e61d92c1fdc727a5a135c277e4d78350fa9c
pressielicious <pressiepotato.exe@gmail.com>
2025-02-25 18:11:02 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,4 +5,63 @@ const map = L.map('map', {
 }).setView([0, 0], -1);
 
 const imageUrl = "map.jpeg";  // Tukar kepada fail peta GTA V
-const imageBounds = [[-8192, -6444], [8192, 6444]];
+const imageBounds = [[-8192, -12888], [8192, 12888]];
+
+L.imageOverlay(imageUrl, imageBounds).addTo(map);
+map.setMaxBounds(imageBounds);
+
+const markers = {};
+let hiddenMarkers = [];
+let lastHiddenMarker = null;
+
+const markerData = [  
+    { id: "marker1", lat: 100, lng: 200 },  
+    { id: "marker2", lat: -300, lng: 150 },  
+    { id: "marker3", lat: 400, lng: -250 }
+];
+
+function loadMarkers() {
+    markerData.forEach(data => {
+        const marker = L.marker([data.lat, data.lng]).addTo(map)
+            .on("contextmenu", () => hideMarker(data.id));
+
+        markers[data.id] = marker;
+    });
+
+    database.ref("hiddenMarkers").on("value", snapshot => {
+        hiddenMarkers = snapshot.val() || [];
+        updateMarkers();
+    });
+}
+
+function hideMarker(id) {
+    if (!hiddenMarkers.includes(id)) {
+        hiddenMarkers.push(id);
+        lastHiddenMarker = id;
+        updateDatabase();
+    }
+}
+
+function updateMarkers() {
+    Object.keys(markers).forEach(id => {
+        if (hiddenMarkers.includes(id)) {
+            map.removeLayer(markers[id]);
+        } else {
+            markers[id].addTo(map);
+        }
+    });
+}
+
+function updateDatabase() {
+    database.ref("hiddenMarkers").set(hiddenMarkers);
+}
+
+document.getElementById("undoBtn").addEventListener("click", () => {
+    if (lastHiddenMarker) {
+        hiddenMarkers = hiddenMarkers.filter(id => id !== lastHiddenMarker);
+        lastHiddenMarker = null;
+        updateDatabase();
+    }
+});
+
+loadMarkers();
