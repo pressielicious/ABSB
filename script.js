b0f62f7205b68297e63195cb1f380b6600ace0da
pressielicious <pressiepotato.exe@gmail.com>
2025-02-26 00:12:10 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,85 @@
-// Inisialisasi peta
+// Tetapan peta dan image overlay
 var map = L.map('map', {
-    crs: L.CRS.Simple,
     minZoom: -2,
-    maxZoom: 2
-});
+    maxZoom: 2,
+    crs: L.CRS.Simple
+}).setView([0, 0], 0);
 
-var mapBounds = [[0, 0], [12888, 8192]]; // Saiz peta
-var image = L.imageOverlay('map.jpeg', mapBounds).addTo(map);
-map.fitBounds(mapBounds);
-map.setMaxBounds(mapBounds);
+var w = 8192, h = 12888;
+var southWest = map.unproject([0, h], map.getMaxZoom());
+var northEast = map.unproject([w, 0], map.getMaxZoom());
+var bounds = new L.LatLngBounds(southWest, northEast);
 
+L.imageOverlay("map.jpeg", bounds).addTo(map);
+map.setMaxBounds(bounds);
+
+// Senarai marker
 var markers = {};
 var hiddenMarkers = [];
+var sheetURL = "https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec";
 
-// Fungsi untuk load marker dari coordinates.json
-async function loadMarkers() {
-    let response = await fetch('coordinates.json');
-    let data = await response.json();
-
-    data.forEach(coord => {
-        let marker = L.marker([coord.y, coord.x]).addTo(map)
-            .bindPopup(coord.name);
-
-        marker.on('contextmenu', function () {
-            map.removeLayer(marker);
-            hiddenMarkers.push(coord);
-            syncToGoogleSheets();
+// Muatkan marker dari marker.json
+fetch('marker.json')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(m => {
+            var marker = L.marker(map.unproject([m.latlng.lng, m.latlng.lat], map.getMaxZoom()))
+                .addTo(map)
+                .on('contextmenu', function () {
+                    hideMarker(m.id);
+                });
+            markers[m.id] = marker;
         });
 
-        markers[coord.id] = marker;
+        // Muatkan marker yang tersembunyi dari Google Sheets
+        fetch(sheetURL + "?action=get")
+            .then(response => response.json())
+            .then(hiddenIds => {
+                hiddenIds.forEach(id => {
+                    if (markers[id]) {
+                        markers[id].remove();
+                    }
+                });
+            });
     });
+
+// Fungsi untuk sembunyikan marker
+function hideMarker(id) {
+    if (markers[id]) {
+        hiddenMarkers.push(id);
+        markers[id].remove();
+        updateSheet(id, "hide");
+    }
 }
 
-// Sync ke Google Sheets
-function syncToGoogleSheets() {
-    let hiddenData = hiddenMarkers.map(m => `${m.id},${m.x},${m.y}`).join("\n");
-    let url = "https://script.google.com/macros/s/AKfycbzyHJh3UraXvawXsoDTyCutZ559RLc7XBs6byPNpdwkj_7hiDEoyn-QXh3lf8ar2nHUrg/exec";
-    fetch(url, {
-        method: "POST",
-        body: JSON.stringify({ hidden: hiddenData })
-    });
-}
-
-// Undo fungsi
-document.getElementById('undoButton').addEventListener('click', function () {
+// Fungsi untuk undo marker terakhir yang di-hide
+document.getElementById("undoBtn").addEventListener("click", function () {
     if (hiddenMarkers.length > 0) {
-        let lastHidden = hiddenMarkers.pop();
-        let marker = L.marker([lastHidden.y, lastHidden.x]).addTo(map)
-            .bindPopup(lastHidden.name);
-        markers[lastHidden.id] = marker;
+        var id = hiddenMarkers.pop();
+        if (markers[id]) {
+            markers[id].addTo(map);
+            updateSheet(id, "undo");
+        }
     }
 });
 
-// Load markers
-loadMarkers();
+// Fungsi untuk tsunami (reset semua marker)
+document.getElementById("tsunamiBtn").addEventListener("click", function () {
+    var password = prompt("Masukkan password:");
+    if (password === "rm40") {
+        hiddenMarkers = [];
+        Object.keys(markers).forEach(id => markers[id].addTo(map));
+        updateSheet(null, "reset");
+    } else {
+        alert("Password salah!");
+    }
+});
+
+// Fungsi update Google Sheets
+function updateSheet(id, action) {
+    fetch(sheetURL, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ action: action, id: id })
+    });
+}
